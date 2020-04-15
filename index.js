96c0d84a42a64759c4171994200fc31e7f53a03b
kwonhygge <kitteniso1103@gmail.com>
2020-04-15 15:34:53 +0900

    Heroku change

--- index.js
+++ index.js
@@ -51,6 +51,6 @@ app.post("/",function(req,res){
 //npm install request
 //request 사용법 -> 구글링
 
-app.listen(3000,function(){
+app.listen(process.env.PORT||3000,function(){
   console.log("Server is running on port 3000.");
 });
